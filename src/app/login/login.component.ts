78857131f63d7003fbbe09b16cb094548cbe9b84
rahul kumar <wiretorahul@gmail.com>
2018-09-29 21:45:05 +0530

    added files

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+  login = { "name": " ", "password": "" };
+  credentials = { "name": 'sourabh', "password": 'sourabh123' };
+  submitted = false;
+  onSubmit() {
+    if (this.login.name === this.credentials.name && this.login.password === this.credentials.password) {
+      alert('Login Successful');
+      this.submitted = true;
+    }
+    else {
+      alert('Login Failed, Please try again ');
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
